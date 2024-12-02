4069507ecd286f586be6389efb7056596d3d38e9
Nandan Pathak <nandanpathak30@gmail.com>
2024-12-03 01:10:57 +0530

    add:login page desing

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,6 +3,7 @@ import Button from "@/components/ui/Button"
 import { useState } from "react"
 import { signIn } from "next-auth/react"
 import { toast } from "react-hot-toast"
+import Image from "next/image"
 
 
 const Login = () => {
@@ -20,49 +21,39 @@ const Login = () => {
         }
     }
 
-    return <>
-        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-            <div className="w-full flex flex-col items-center max-w-md space-y-8 ">
-                <div className="flex flex-col items-center gap-8">
-                    logo
-                    <div className="mt-6 text-center text-3xl font-bold tracking-tighter text-gray-900">Sign in to your account</div>
-                </div>
-                <Button isLoading={isLoading} type="button"
-                    className="max-w-sm mx-auto w-full"
-                    onClick={loginWithGoogle}>
-                    {isLoading ? null : <svg
-                        className='mr-2 h-4 w-4'
-                        aria-hidden='true'
-                        focusable='false'
-                        data-prefix='fab'
-                        data-icon='github'
-                        role='img'
-                        xmlns='http://www.w3.org/2000/svg'
-                        viewBox='0 0 24 24'>
-                        <path
-                            d='M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z'
-                            fill='#4285F4'
-                        />
-                        <path
-                            d='M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z'
-                            fill='#34A853'
-                        />
-                        <path
-                            d='M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z'
-                            fill='#FBBC05'
-                        />
-                        <path
-                            d='M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z'
-                            fill='#EA4335'
-                        />
-                        <path d='M1 1h22v22H1z' fill='none' />
-                    </svg>
-                    }
+    return <div className="flex md:flex-row flex-col min-h-[100dvh]">
 
-                    Google
-                </Button>
+        <div className="flex-[1.5] bg-[#925FE2] flex flex-col bg-[url('/images/object.svg')] bg-center px-20 w-full">
+            <div className="flex flex-col pt-20 md:pt-24 space-y-2">
+                <p className="text-4xl sm:text-5xl text-white poppins-bold">Welcome</p>
+                <p className="text-4xl sm:text-6xl text-white poppins-regular">to the ChatBox</p>
+                <p className="text-white poppins-regular">Login to access you account</p>
+            </div>
+            <div className="md:flex hidden justify-end pr-20">
+                <Image
+                    src="/images/hero-image.png"
+                    alt="hero-login-image-icon"
+                    width={450}
+                    height={450}
+                />
             </div>
         </div>
-    </>
+
+        <div className="flex-1 bg-[rgb(28,29,33)] flex flex-col justify-center items-center space-y-5">
+            <div className="flex flex-col items-center">
+                <h1 className="text-gray-300 text-3xl poppins-bold">Login</h1>
+                <p className="text-gray-500 poppins-regular">Secure authentication using Google</p>
+            </div>
+            <Button isLoading={isLoading} type="button"
+                className="max-w-mg mx-auto bg-white text-black hover:bg-gray-300 px-20"
+                onClick={loginWithGoogle}>
+                {isLoading ? null : <Image src="/icon/google-icon.svg" alt="google-icon" width={16} height={16} />
+                }
+                <span className="pl-2 poppins-regular">
+                    Google
+                </span>
+            </Button>
+        </div>
+    </div>
 }
 export default Login;
